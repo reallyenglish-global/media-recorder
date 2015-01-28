12708e6cddf3003ba1d3b74b15a8cb4f2bfbbe94
joey <joeyw@reallyenglish.com>
2015-01-28 18:03:12 +0000

    export in WAV format

--- src/RecorderHtml5.js
+++ src/RecorderHtml5.js
@@ -1,3 +1,65 @@
+function floatTo16BitPCM(output, offset, input){
+  for (var i = 0; i < input.length; i++, offset+=2){
+    var s = Math.max(-1, Math.min(1, input[i]));
+    output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
+  }
+}
+function writeString(view, offset, string){
+  for (var i = 0; i < string.length; i++){
+    view.setUint8(offset + i, string.charCodeAt(i));
+  }
+}
+function interleave(inputL, inputR){
+  var length = inputL.length + inputR.length;
+  var result = new Float32Array(length);
+
+  var index = 0,
+    inputIndex = 0;
+
+  while (index < length){
+    result[index++] = inputL[inputIndex];
+    result[index++] = inputR[inputIndex];
+    inputIndex++;
+  }
+  return result;
+}
+
+function encodeWAV(samples, sampleRate){
+  var buffer = new ArrayBuffer(44 + samples.length * 2);
+  var view = new DataView(buffer);
+
+  /* RIFF identifier */
+  writeString(view, 0, 'RIFF');
+  /* RIFF chunk length */
+  view.setUint32(4, 36 + samples.length * 2, true);
+  /* RIFF type */
+  writeString(view, 8, 'WAVE');
+  /* format chunk identifier */
+  writeString(view, 12, 'fmt ');
+  /* format chunk length */
+  view.setUint32(16, 16, true);
+  /* sample format (raw) */
+  view.setUint16(20, 1, true);
+  /* channel count */
+  view.setUint16(22, 2, true);
+  /* sample rate */
+  view.setUint32(24, sampleRate, true);
+  /* byte rate (sample rate * block align) */
+  view.setUint32(28, sampleRate * 4, true);
+  /* block align (channel count * bytes per sample) */
+  view.setUint16(32, 4, true);
+  /* bits per sample */
+  view.setUint16(34, 16, true);
+  /* data chunk identifier */
+  writeString(view, 36, 'data');
+  /* data chunk length */
+  view.setUint32(40, samples.length * 2, true);
+
+  floatTo16BitPCM(view, 44, samples);
+
+  return view;
+}
+
 var RecorderHtml5 = function(options) {
   this.initialize.call(this, options);
 };
@@ -27,6 +89,7 @@ RecorderHtml5.prototype.initialize = function(cfg) {
 };
 
 RecorderHtml5.prototype.prepare = function prepare(stream) {
+  this.stream = stream;
   this.input = this.audio_context.createMediaStreamSource(stream);
   this.context = this.input.context;
   this.node = (this.context.createScriptProcessor ||
@@ -86,4 +149,11 @@ RecorderHtml5.prototype.play = function play() {
 
 RecorderHtml5.prototype.stop = function stop() {
   this.recording = false;
+};
+
+RecorderHtml5.prototype.getData = function getData(){
+  var buffers = this.getBuffer();
+  var interleaved = interleave(buffers[0], buffers[1]);
+  var dataview = encodeWAV(interleaved, this.sampleRate);
+  return new Blob([dataview], { type: 'audio/wav' });
 };
