ac5644f950a80a136b2e2911095322afbcaefd9f
Stephen Bartholomew <stephenbartholomew@gmail.com>
2016-01-06 21:30:23 +0000

    Quick working API for recorderjs adapter & demo UI

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,5 +1,22 @@
 var Recorder = require('../../lib/media-recorder');
 
-console.log(Recorder);
+var recorder = new Recorder();
 
+document.addEventListener("DOMContentLoaded", function() {
+  console.log('document loaded');
+  var play = document.getElementById('play');
+  var record = document.getElementById('record');
+  var stop = document.getElementById('stop');
 
+  record.addEventListener('click', function(e) {
+    recorder.startRecording();
+  });
+
+  stop.addEventListener('click', function(e) {
+    recorder.stopRecording();
+  });
+
+  play.addEventListener('click', function(e) {
+    recorder.startPlayback();
+  });
+});
