676bd06d026117409bde16f7028daee633ae5d2c
Stephen Bartholomew <stephenbartholomew@gmail.com>
2016-01-12 11:33:03 +0000

    Interface test but not much more

---
+++ test/lib/adapters/recorderjs.spec.js
@@ -0,0 +1,27 @@
+window = {
+  AudioContext: {}
+};
+
+var RecorderJsAdapter = require('../../../lib/adapters/recorderjs');
+var adapterApi = ['startRecording', 'stopRecording', 'startPlaying', 'stopPlaying', 'isRecording', 'canPlay']; 
+var _ = require('underscore');
+var sinon = require('sinon');
+
+describe('RecorderJsAdapter', function() {
+  var adapter, observer;
+
+  before(function() {
+    observer = {
+      onStoppedPlaying: sinon.spy()
+    };
+
+    adapter = new RecorderJsAdapter();  
+    adapter.addObserver(observer);
+  });
+
+  it('supports the adapter interface', function() {
+    _.each(adapterApi, function(name) {
+      expect(typeof adapter[name]).to.eql('function');
+    });
+  });
+});
