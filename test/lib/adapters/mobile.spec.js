2f27ba8309b41f73404ff41b73644729e563d503
Stephen Bartholomew <stephenbartholomew@gmail.com>
2016-09-02 08:01:19 +0100

    fix(mobile): set state correctly for both playback stopped events, specs

--- test/lib/adapters/mobile.spec.js
+++ test/lib/adapters/mobile.spec.js
@@ -3,6 +3,10 @@ var sinon = require('sinon');
 var adapterApi = ['startRecording', 'stopRecording', 'startPlaying', 'stopPlaying', 'reset'];
 var _ = require('underscore');
 
+var STOPPED = 1,
+    PLAYING = 2,
+    RECORDING = 3
+
 var fakeRecorder = function() {};
 
 _.extend(fakeRecorder.prototype, {
@@ -15,20 +19,24 @@ _.extend(fakeRecorder.prototype, {
   reset: sinon.spy()
 });
 
+var setup = function() {
+  window.rels = {
+    mobile: {
+      recorder: fakeRecorder
+    }
+  };
+}
+
+var teardown = function() {
+  delete window.rels;
+}
+
+
 describe('MobileAdapter', function() {
   describe('isSupported', function() {
     context('client supports mobile app recording', function() {
-      before(function() {
-        window.rels = {
-          mobile: {
-            recorder: {}
-          }
-        };
-      });
-
-      after(function() {
-        delete window.rels;
-      });
+      before(setup);
+      after(teardown);
 
       it('returns true', function() {
         expect(MobileAdapter.isSupported()).to.be.true;
@@ -46,22 +54,43 @@ describe('MobileAdapter', function() {
     var adapter;
 
     before(function() {
-      window.rels = {
-        mobile: {
-          recorder: fakeRecorder
-        }
-      };
+      setup();
       adapter = new MobileAdapter()
     });
 
-    after(function() {
-      delete window.rels;
-    });
+    after(teardown);
 
     it('supports the adapter interface', function() {
       _.each(adapterApi, function(name) {
         expect(_.bind(adapter[name], adapter)).to.not.throw(Error);
       });
+    });
+  });
+
+  describe('onPlaybackEnded', function() {
+    var adapter,
+        observer = {
+          onStoppedPlaying: sinon.spy()
+        };
+
+    before(function() {
+      setup();
+      adapter = new MobileAdapter();
+      adapter.addObserver(observer, ['stopped-playing']);
+
+      adapter.startPlaying();
+
+      adapter.onPlaybackEnded();
+    });
+
+    after(teardown);
+
+    it('notifies', function() {
+      expect(observer.onStoppedPlaying).to.have.been.called;
+    });
+
+    it('sets the correct state', function() {
+      expect(adapter.state).to.eql(STOPPED);
     });
   });
 });
